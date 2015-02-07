c7c301bf97cf42d099b421dbf0c75f816a22ee8d
Rich Hildred <rhildred@gmail.com>
2015-02-06 22:52:54 -0500

    thank-you for your inquiry

--- Bitbucket.php
+++ Bitbucket.php
@@ -58,7 +58,7 @@ class Bitbucket{
             $response = json_decode($response);
             if(isset($response->local_id) && intval($response->local_id) > 0 ){
                 $bugurl = "https://bitbucket.org/". $bbRepo . "/issue/" . $response->local_id;
-                return( array('issueid'=>$response->local_id, 'issueurl'=>$bugurl, 'result'=>'success') );
+                return( array('issueid'=>$response->local_id, 'issueurl'=>$bugurl, 'result'=>'thank-you for your question') );
             }else{
                 return(FALSE);
             }
